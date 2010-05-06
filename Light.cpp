be0d6cdac37740fecf599c5753a45a96bc00e275
Keishi Hattori <keishi@webkit.org>
2010-05-06 21:45:40 +0900

    add Light

---
+++ Light.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Light.cpp
+ *  ShadeKit
+ *
+ *  Created by Keishi Hattori on 5/6/10.
+ *  Copyright 2010 Keishi Hattori. All rights reserved.
+ *
+ */
+
+#include "Light.h"
+
+namespace ShadeKit {
+    
+}
---
+++ Light.h
@@ -0,0 +1,33 @@
+/*
+ *  Light.h
+ *  ShadeKit
+ *
+ *  Created by Keishi Hattori on 5/6/10.
+ *  Copyright 2010 Keishi Hattori. All rights reserved.
+ *
+ */
+
+#ifndef Light_h
+#define Light_h
+
+#include "Color.h"
+#include "Vector3.h"
+
+namespace ShadeKit {
+    typedef enum {
+        PKLightAttenuationNone = 0,
+        PKLightAttenuationLinear = 1,
+        PKLightAttenuationQuadratic = 2
+    } LightAttenuation;
+    class Light {
+    public:
+        Light() : m_intensity(1.0f), m_attenuation(PKLightAttenuationNone) { }
+    private:
+        Vector3 m_position;
+        Color m_color;
+        float m_intensity;
+        LightAttenuation m_attenuation;
+    };
+}
+
+#endif
